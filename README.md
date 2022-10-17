ac90059a7aa886e7a6ba42056f0927f9c1776872
tbswang <gt2017qd@163.com>
2022-10-17 21:50:49 +0800

    feat: http repsponse

---
+++ README.md
@@ -0,0 +1,28 @@
+SWIMLINE
+
+a static file distribution engine like Nginx
+
+## Installation
+
+
+## How to use
+
+swimline.config.json
+
+```json
+[
+  {
+    "entry":"/demo",
+    "priority": 0,
+    "target": "<path to the file: if relative, base the json file>"
+  },
+
+]
+
+```
+
+## Compare
+### vs Nginx 
+* config file with module reference
+* 
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("start");
+    let listener = TcpListener::bind("127.0.0.1:2022").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("------stream start--------");
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("{}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK \r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("------stream end--------");
 }
