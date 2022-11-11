795d87fc108bfd2f1997ee0a4b28187b7fa40567
tbswang <gt2017qd@163.com>
2022-11-11 19:49:45 +0800

    feat: response file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{Read, Write},
+    fs,
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -13,13 +14,41 @@ fn main() {
     }
 }
 
+// fn handle_connection(mut stream: TcpStream) {
+//     println!("------stream start--------");
+//     let mut buffer = [0; 1024];
+//     stream.read(&mut buffer).unwrap();
+//     println!("{}", String::from_utf8_lossy(&buffer[..]));
+//     let response = "HTTP/1.1 200 OK \r\n\r\n";
+//     stream.write(response.as_bytes()).unwrap();
+//     stream.flush().unwrap();
+//     println!("------stream end--------");
+// }
+
 fn handle_connection(mut stream: TcpStream) {
-    println!("------stream start--------");
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-    println!("{}", String::from_utf8_lossy(&buffer[..]));
-    let response = "HTTP/1.1 200 OK \r\n\r\n";
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    println!("------stream end--------");
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    print!("Request: {:#?}", http_request);
+
+    // let response = "HTTP/1.1 200 OK \r\n\r\n ad=asdfas \r\n\r\n";
+    // stream.write(response.as_bytes()).unwrap();
+    // stream.flush().unwrap();
+    // println!("----- steam end ------")
+
+    // path is relative to cargo.toml
+    let content = fs::read_to_string("./hello.html").unwrap();
+    let status_line = "HTTP/1.1 200 OK";
+    let length = content.len();
+
+    let response = format!(
+        "{}\r\nContent-Length:{length}\r\n\r\n{content}",
+        status_line
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
+
