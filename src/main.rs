f8348487b9a6352579bea44e6377005d25e9d88d
tbswang <gt2017qd@163.com>
2022-11-28 14:27:51 +0800

    feat: single thread web server

--- src/main.rs
+++ src/main.rs
@@ -5,50 +5,25 @@ use std::{
 };
 
 fn main() {
-    println!("start");
-    let listener = TcpListener::bind("127.0.0.1:2022").unwrap();
-
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream)
     }
 }
 
-// fn handle_connection(mut stream: TcpStream) {
-//     println!("------stream start--------");
-//     let mut buffer = [0; 1024];
-//     stream.read(&mut buffer).unwrap();
-//     println!("{}", String::from_utf8_lossy(&buffer[..]));
-//     let response = "HTTP/1.1 200 OK \r\n\r\n";
-//     stream.write(response.as_bytes()).unwrap();
-//     stream.flush().unwrap();
-//     println!("------stream end--------");
-// }
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    print!("Request: {:#?}", http_request);
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "hello.html")
+    };
 
-    // let response = "HTTP/1.1 200 OK \r\n\r\n ad=asdfas \r\n\r\n";
-    // stream.write(response.as_bytes()).unwrap();
-    // stream.flush().unwrap();
-    // println!("----- steam end ------")
-
-    // path is relative to cargo.toml
-    let content = fs::read_to_string("./hello.html").unwrap();
-    let status_line = "HTTP/1.1 200 OK";
-    let length = content.len();
-
-    let response = format!(
-        "{}\r\nContent-Length:{length}\r\n\r\n{content}",
-        status_line
-    );
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length:{length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
-
