fa8a7eb20b2ed8de3c3b0bf8e55cf291cc59a7c3
tbswang <gt2017qd@163.com>
2022-11-29 23:06:01 +0800

    feat: try thread

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ use std::{
     fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream)
+        thread::spawn(|| {
+            handle_connection(stream);
+        })
     }
 }
 
